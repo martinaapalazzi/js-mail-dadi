e5875dcdea52fb769d4b88386ce9f53ac870ab81
martinaapalazzi <martinaapalazzi@gmail.com>
2023-12-06 15:16:42 +0100

    Inizializzazione esercizio dadi + math.random

---
+++ dadi.esercizio/script.js
@@ -0,0 +1,15 @@
+/*
+Dadi esercizio:
+
+- Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+- Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+Domande:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+*/ 
+
+const userNumberRandom = Math.floor(Math.random() * (100 - 0 + 1) + 0)
+
+console.log (userNumberRandom)
