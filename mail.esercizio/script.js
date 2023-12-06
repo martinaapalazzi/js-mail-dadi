d365e64d5a8aa7b498d6a50646a2cba03e4a8895
martinaapalazzi <martinaapalazzi@gmail.com>
2023-12-06 15:08:44 +0100

    user email prompt

---
+++ mail.esercizio/script.js
@@ -0,0 +1,13 @@
+/*
+Mail esercizio:
+
+- Chiedi all'utente la sua email
+- Controlla che sia nella lista di chi può accedere.
+- Stampa un messaggio appropriato sull'esito del controllo.
+
+*/ 
+
+
+const userEmail = prompt ("Qual'è la tua email?")
+
+console.log ('userEmail', userEmail, typeof userEmail)
